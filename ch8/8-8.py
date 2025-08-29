9f0b203f46e5d0538efa7b5ef7eaf595a327de35
photonhead <photonhead37@gmail.com>
2025-08-29 11:06:09 -0700

    add ch8 file (8/27, 8/29, 8-8.py)

--- ch8/8-8.py
+++ ch8/8-8.py
@@ -1,66 +1,56 @@
 '''
-8-8. User Albums:
-Start with your program from Ex 8-7.
+8-8. User Albums: Start with your program from Ex 8-7.
 Write a while loop that allows users to enter an album's artist and title.
-Once you have that information, call make_album() with the user's input
-and print the dictionary that's created.
+Once you have that information, call make_album() with the user's input and print the dictionary that's created.
 Be sure to include a quit value in the while loop.
 '''
-# define a function make_album()
-def make_album(artist_name, album_title, num_songs=None):
-    '''Album information dictionary with optional number of songs.'''
-    album_info = {
-        'Artist': artist_name, 
-        'Album': album_title, 
-        'Number of Songs': num_songs
-        }
+def make_album():
+    active = True # to break in while loop
 
-    # --- while loop begins ----
-    active = True
-
+    # --- while loop begins --- 
     while active:
-        '''Get the user input for an album's artist, title, and number of songs (optional)'''
-        prompt = "\nTo add your favorite album, please provide the following information "
-        prompt += "(Artist name is case-sensitive. Enter 'q' at any time to quit.)"
+        prompt = "\nPlease add album details to the database. "
+        prompt += "Artist name is case-sensitive, and type in order first name and last name (if needed)."
+        prompt += "\n(Press 'q' when you want to stop.)\n"
         print(prompt)
 
-        artist_name = input("Aritst name: ")
-        if artist_name == 'q': # quit - stop while loop
+        artist = input("- Artist name?: ")
+        if artist == 'q': # when user input is 'q', 
+            active = False # end the while loop
+            break
+
+        album = input("- Album title?: ").title() # album title format
+        if album == 'q':
             active = False
             break
 
-        album_title = input("Album title: ")
-        if album_title == 'q': # quit - stop while loop
+        num_songs = input("- Number of songs?: ") # String type
+        if num_songs == '0': # when user input is '0', change it to None
+            num_songs = None
+        elif num_songs == 'q':
             active = False
             break
-
-        num_songs = input("Number of songs (optional, type 0 to skip): ")
-        if num_songs == 0: # if 0, add it as None to the dictionary
-            num_songs = None        
-        elif num_songs == 'q': # quit - stop while loop
-            active = False
-            break
-
-        '''Add user input information to the dictionary'''
-        album_info['Artist'] = artist_name
-        album_info['Album'] = album_title
-        album_info['Number of Songs'] = num_songs
+        else:
+            num_songs == int(num_songs) # change type from String to integer
         
-        '''Let user know the album is added to the dictionary'''
-        confirm_msg = f"\nYour album has been added successfully."
-        print(confirm_msg)
-    # --- while loop finished. ----
-    
-    '''Get each values for artist, album title, and num_songs from dictionary.'''
-    artist = album_info['Artist']
-    album = album_info['Album']
-    num_songs = album_info['Number of Songs']
+        database = {
+            'Artist': artist, 
+            'Album': album, 
+            'Number of Songs': num_songs}
+        
+        # print(database) # dictionary input test
 
-    if num_songs != None:
-        full_album_info = f"<{album.title()}> by {artist} ({num_songs} songs)"
-    else:
-        full_album_info = f"<{album.title()}> by {artist}"
+        for album_info in database.items():
+            ## album_info formats 
+            if num_songs == None:
+                album_info = f"\'{album}\' by {artist}"
+            else:
+                album_info = f"\'{album}\' by {artist} ({num_songs} songs)"
+                
+        print(album_info)
+    # --- while loop finished! --- 
 
-    return(full_album_info)
+    print("Done. Thank you!") # when user input is 'q', show this message
 
+# call the function
 make_album()
